92963d27a02a144c05397630da6e869a94b159fa
laurelstokely <laurel.stokely@gmail.com>
2024-10-08 19:13:49 -0700

    img alter
    changed img size and cover

--- script.js
+++ script.js
@@ -1,37 +1,56 @@
-function generateRandomNumber (num) {
-    //Gets random Number from 0 - 1
-    return Math.floor(Math.random() * num)
-}
+const words = new URLSearchParams(window.location.search);
 
-const collectiveWisdom = {
-    godInfo: ['Isis', 'Horus', 'Bastet', 'Ra', 'Set'],
-    fortuneOutput: ['Set has torn out your eyes', 'Isis knows your secret name', 'Ra has taken pitty on you'],
-    advice: ['Hide in the papyrus', 'Fight back with magic', 'make a mummy']
-}
-//Store the 'wisdom in an array
-let personalWisdom = []
+const godOrGoddess = words.get('godOrGoddess');
+const region = words.get('region');
+const hero = words.get('hero');
 
-//Iterate over the object
-for(let prop in collectiveWisdom) {
-    let opinionIdx = generateRandomNumber(collectiveWisdom[prop].length)
-    //use the object's properties to customize the message being added
-    switch(prop) {
-        case 'godInfo':
-            personalWisdom.push('The god affecting you right now is "${collectiveWisdom[prop][optionIdx]}".')
-            break
-        case 'fortuneOutput':
-            personalWisdom.push('Your current situation is: "${collectiveWisdom[prop][optionIdx]}".')
-            break
-        case 'advice':
-            personalWisdom.push('You should: "{collectiveWidsom[prop][optionIdx]}".')
-            break
-        default:
-            personalWisdom.push('There is not enough info.')
+function getRandNum(array){
+    return Math.floor(Math.random() * array.length);
+};
+
+const storyBegin = ['On an early morning strole',
+    'After a long day of work', 
+    'After a mid-morning snack',
+    'Just before the early morning rays',
+    'In the dead of night'
+];
+
+let start = storyBegin[getRandNum(storyBegin)];
+
+const storyBody = ['Was confronted by an angy herd of Hippopotamouses. That were extra aggressive due to the drought',
+    'Was forced to answer the riddle of the Sphinx.',
+    'Came across an ornate box wedged amongst the trees at the edge of the river Inside were the cut up remains of Osiris.',
+    'Was faced with the Lioness form of the goddess. She was charged with decimating a village and was currently rampaging out of control.',
+    'Was tasked by the god with unifying all of Kemet.'
+];
+
+let body = storyBody[getRandNum(storyBody)];
+
+const storyEnd = ['Was wrapped in linen to become the first mummy.',
+    'Answered her questions three and was granted with tremendous knowledge.',
+    'Called upon the river god Sobek for protection.',
+    'Colored the beer red thus satiating the goddess blood lust and making her drunk.',
+    'Took up the Crook and Flail and gathered up great armies.'
+];
+
+let end = storyEnd[getRandNum(storyEnd)];
+
+function createStory() {
+    function getRandNum(array){
+        return Math.floor(Math.random() * array.length);
     }
-}
+    let start = storyBegin[getRandNum(storyBegin)];
+    let body = storyBody[getRandNum(storyBody)];
+    let end = storyEnd[getRandNum(storyEnd)];
 
-function formatWisdom(wisdom) {
-    const formatted = personalWisdom.join('/n')
-    console.log(formatted)
+    const fullStory = `${start}, our hero ${hero} Ran into ${godOrGoddess}. ${body} And in response ${end}.`;
+    console.log(fullStory);
+    return fullStory;
+};
+
+function addStory() {
+    document.getElementById("story").innerHTML +=
+    `<p>
+        ${start}, our hero ${hero} ran into ${godOrGoddess}. ${body} And in response ${end}.
+    </p>`;
 }
-formatWisdom(personalWisdom);
